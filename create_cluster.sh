1f9c3348cfe8d26452d27b2924a7fe2ea2f1e8cb
rodybothe <rody.bothe@dyflexis.com>
2024-01-15 10:40:51 +0100

    added eks

---
+++ create_cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+eksctl create cluster \
+--name demo-cluster2 \
+--version 1.27 \
+--region eu-central-1 \
+--nodegroup-name demo-nodes \
+--node-type t2.micro \
+--nodes 2 \
+--nodes-min 1 \
+--nodes-max 3
